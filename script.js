920ea1ee3e58b040041f2dc32071a813d86d9d89
Parth Verma <parth50verma@gmail.com>
2023-06-15 21:56:59 +0530

    added laps functionality.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     let startTime = null;
     let elapsed = 0;
     let inter = 0;
+    let laptext = '';
+    let lapcount = 0;
+    let lastlap = 0;
     var content = $('#main');
     function startStopwatch(){
         startTime = performance.now() - elapsed;
@@ -22,7 +25,7 @@ $(document).ready(function(){
     function resetStopwatch(){
         clearInterval(inter);
         elapsed = 0;
-        content.text(elapsed.toFixed(2));
+        content.text(elapsed.toFixed(3));
         
     }
 
@@ -36,22 +39,39 @@ $(document).ready(function(){
             startStopwatch();
             $('#pause-resume-button').text('Pause');
             $(this).fadeOut(100,function(){
-                $('#reset-button, #pause-resume-button').fadeIn(100);
+                $('#reset-lap-button, #pause-resume-button').fadeIn(100);
             });
             
         }
     });
 
-    $('#reset-button').click(function(){
-        $('#reset-button, #pause-resume-button').fadeOut(250,function(){
-            $('#start-button').fadeIn(250);
-        });
-        resetStopwatch();
+    $('#reset-lap-button').click(function(){
+        if($(this).text() === 'Reset'){
+            $('#reset-lap-button, #pause-resume-button').fadeOut(250,function(){
+                $('#start-button').fadeIn(250);
+                $('#reset-lap-button').text('Lap');
+            });
+            lapcount = 0;
+            laptext = '';
+            lastlap = 0;
+            $('#lap').text(laptext);
+            resetStopwatch();
+        }
+        else{
+            lapcount++;
+            laptime = elapsed - lastlap;
+            const padLapcount = lapcount.toString().padStart(2, '0');
+            laptext =   `<div><span>Lap ${padLapcount}</span><span>:</span> <span>${(laptime/1000).toFixed(3)}${laptext}</span></div>`; 
+            $('#lap').html(laptext) 
+            lastlap = elapsed
+        }
+        
     });
 
     $('#pause-resume-button').click(function(){
         if($(this).text() === 'Pause'){
             $(this).text('Resume');
+            $('#reset-lap-button').text('Reset');
             pauseStopwatch();
         }
         else{
